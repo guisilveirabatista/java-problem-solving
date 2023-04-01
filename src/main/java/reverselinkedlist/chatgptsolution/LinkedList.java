29aa1567fc115ada13841799bff21d4037159b89
Guilherme Silveira <guilherme.silveira@ing.com>
2023-04-01 21:41:45 +0200

    Adding chatgpt solution

---
+++ src/main/java/reverselinkedlist/chatgptsolution/LinkedList.java
@@ -0,0 +1,69 @@
+package reverselinkedlist.chatgptsolution;
+
+public class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        list.reverse();
+
+        System.out.println("Reversed List:");
+        list.printList();
+    }
+}
